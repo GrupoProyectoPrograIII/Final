b70360666f94771ae2ecf074ff3a1d187d62367e
Antonio <56178282+Antonio2k19@users.noreply.github.com>
2021-06-11 02:23:44 -0600

    Movimiento Crud y Dao

--- webServiceFinal/src/java/org/dao/DaoMovimiento.java
+++ webServiceFinal/src/java/org/dao/DaoMovimiento.java
@@ -2,22 +2,27 @@ package org.dao;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.config.Conexion;
 import org.crud.CrudMovimiento;
+import org.modelo.Cuenta;
 import org.modelo.Movimiento;
 
-public class DaoMovimiento implements CrudMovimiento{
-    
+public class DaoMovimiento implements CrudMovimiento {
+
     String strSql = "";
+    String strSql2 = "";
     Movimiento mov = new Movimiento();
     Conexion conexion = new Conexion();
     Connection con;
     ResultSet rs = null;
-    
+    boolean respuesta = false;
+
     @Override
     public List listar() {
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -26,12 +31,12 @@ public class DaoMovimiento implements CrudMovimiento{
         try {
             conexion.open();
             rs = conexion.executeQuery(strSql);
-            while(rs.next()){
+            while (rs.next()) {
                 Movimiento movimiento = new Movimiento();
                 movimiento.setFechaMovimiento(rs.getTimestamp("FEC_MOVIMIENTO"));
                 movimiento.setCuenta(rs.getString("NUMERO_CUENTA"));
                 movimiento.setNombre(rs.getString("NOMBRE"));
-                movimiento.setApellido(rs.getString("APELLIDO"));                
+                movimiento.setApellido(rs.getString("APELLIDO"));
                 movimiento.setMovimiento(rs.getString("DESCRIPCION"));
                 movimiento.setTipoCambio(rs.getDouble("TIPO_CAMBIO"));
                 movimiento.setSaldoQ(rs.getDouble("SALDO_Q"));
@@ -39,10 +44,10 @@ public class DaoMovimiento implements CrudMovimiento{
                 lstMovimiento.add(movimiento);
             }
             rs.close();
-            conexion.close();            
-        } catch(ClassNotFoundException ex){
+            conexion.close();
+        } catch (ClassNotFoundException ex) {
             Logger.getLogger(DaoMovimiento.class.getName()).log(Level.SEVERE, null, ex);
-        } catch(Exception ex){
+        } catch (Exception ex) {
             Logger.getLogger(DaoMovimiento.class.getName()).log(Level.SEVERE, null, ex);
         }
         return lstMovimiento;
@@ -54,8 +59,39 @@ public class DaoMovimiento implements CrudMovimiento{
     }
 
     @Override
-    public Movimiento insertar(Movimiento movimiento) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public boolean insertar(Movimiento movimiento) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Cuenta cuenta = new Cuenta();
+        try {
+            conexion.open().setAutoCommit(false);
+            Statement stmt = conexion.open().createStatement();
+            strSql = "INSERT INTO MOVIMIENTOS (FEC_MOVIMIENTO,ID_CLIENTE,ID_CUENTA,TIPO_MOVIMIENTO,USUARIO,TIPO_CAMBIO,SALDO_Q,SALDO_D) VALUES (CURRENT_TIMESTAMP," + movimiento.getIdCliente() + ", " + movimiento.getIdCuenta() + ", " + movimiento.getTipoMovimiento() + ",'" + movimiento.getUsuario() + "', " + movimiento.getTipoCambio() + ", " + movimiento.getSaldoQ() + ", " + movimiento.getSaldoD() + ")";
+            stmt.executeUpdate(strSql);
+            cuenta = selectCuenta(movimiento.getIdCuenta());
+            if (movimiento.getTipoMovimiento() == 1) {
+                strSql2 = "UPDATE CUENTA SET ID_CLIENTE = " + cuenta.getIdCuenta() + ", NUMERO_CUENTA = '" + cuenta.getNumeroCuenta() + "', TIPO_CUENTA = " + cuenta.getTipoCuenta() + ", SALDO_Q = " + (cuenta.getSaldoQ() - movimiento.getSaldoQ()) + ", SALDO_D = " + (cuenta.getSaldoQ() - movimiento.getSaldoD()) + " WHERE ID_CUENTA = " + cuenta.getIdCuenta();
+            } else {
+                strSql2 = "UPDATE CUENTA SET ID_CLIENTE = " + cuenta.getIdCuenta() + ", NUMERO_CUENTA = '" + cuenta.getNumeroCuenta() + "', TIPO_CUENTA = " + cuenta.getTipoCuenta() + ", SALDO_Q = " + (cuenta.getSaldoQ() + movimiento.getSaldoQ()) + ", SALDO_D = " + (cuenta.getSaldoQ() + movimiento.getSaldoD()) + " WHERE ID_CUENTA = " + cuenta.getIdCuenta();
+            }
+            stmt.executeUpdate(strSql2);
+            conexion.open().commit();
+            conexion.close();
+            respuesta = true;
+        } catch (SQLException se) {
+            try {
+                conexion.open().rollback();
+                respuesta = false;
+            } catch (ClassNotFoundException ex) {
+                Logger.getLogger(DaoMovimiento.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (SQLException ex) {
+                Logger.getLogger(DaoMovimiento.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DaoMovimiento.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(DaoMovimiento.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return respuesta;
     }
 
     @Override
@@ -63,10 +99,32 @@ public class DaoMovimiento implements CrudMovimiento{
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    
     @Override
     public List busqueda(String parametro, String opcion) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
+    public Cuenta selectCuenta(int id) {
+        Cuenta cuenta = new Cuenta();
+        try {
+            strSql = "SELECT * FROM CUENTA WHERE ID_CUENTA=" + id;
+            conexion.open();
+            rs = conexion.executeQuery(strSql);
+            while (rs.next()) {
+                cuenta.setIdCliente(rs.getInt("ID_CLIENTE"));
+                cuenta.setIdCuenta(rs.getInt("ID_CUENTA"));
+                cuenta.setNumeroCuenta(rs.getString("NUMERO_CUENTA"));
+                cuenta.setTipoCuenta(rs.getInt("TIPO_CUENTA"));
+                cuenta.setSaldoQ(rs.getDouble("SALDO_Q"));
+                cuenta.setSaldoD(rs.getDouble("SALDO_D"));
+            }
+            rs.close();
+            conexion.close();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DaoCuenta.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(DaoCuenta.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return cuenta;
+    }
 }
